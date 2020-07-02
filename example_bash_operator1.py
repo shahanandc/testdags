0c032aa3068aa5fc6dd9bb1496c387170fe08154
shahanandc <shahanandc@gmail.com>
2020-07-02 16:24:08 -0500

    Update example_bash_operator1.py

--- example_bash_operator1.py
+++ example_bash_operator1.py
@@ -7,7 +7,7 @@ from airflow.operators.dummy_operator import DummyOperator
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': DAG.utils.dates.days_ago(2),
+    'start_date': 2020-01-01,
     'email': ['airflow@example.com'],
     'email_on_failure': False,
     'email_on_retry': False,
