e1ce87daf3176ad8f9e140e875ebb61fb6645b11
shahanandc <shahanandc@gmail.com>
2020-07-05 19:39:56 -0500

    Update example_bash_operator1.py

--- dags/example_bash_operator1.py
+++ dags/example_bash_operator1.py
@@ -40,7 +40,7 @@ start = BashOperator(task_id='run_this_first_1',
                               ]                              
                           }
                       },
-                      bash_command="sleep 60",
+                      bash_command="mkdir /mnt/cpmodeldata/ModelData/a",
                       xcom_push=True,
                       dag=dag)
 
