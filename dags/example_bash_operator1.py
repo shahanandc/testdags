ba01e475e5e5d38290f145a8e8843ea76562b623
shahanandc <shahanandc@gmail.com>
2020-07-05 19:36:51 -0500

    Update example_bash_operator1.py

--- dags/example_bash_operator1.py
+++ dags/example_bash_operator1.py
@@ -40,7 +40,7 @@ start = BashOperator(task_id='run_this_first_1',
                               ]                              
                           }
                       },
-                      bash_command="ls ../../../mnt/",
+                      bash_command="sleep 60",
                       xcom_push=True,
                       dag=dag)
 
