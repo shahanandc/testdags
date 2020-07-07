f9f20fc55a3973b25cf33a2c020a92864517653e
shahanandc <shahanandc@gmail.com>
2020-07-07 12:59:37 -0500

    Update example_bash_operator1.py

--- dags/example_bash_operator1.py
+++ dags/example_bash_operator1.py
@@ -188,8 +188,150 @@ io_3 = KubernetesPodOperator(namespace='default',
                           dag=dag
                           )
 
+
+io_4 = KubernetesPodOperator(namespace='default',
+                            executor_config={                            
+                              "KubernetesExecutor": {
+                                  "volumes": [
+                                      {
+                                       "name": "cpnprdazurefile",
+                                       "azureFile" :
+                                       {
+                                          "secretName":"cpnprdfilesharepv",
+                                          "shareName":"cpmodeldata"
+                                       }
+                                      }
+                                  ],
+                                  "volume_mounts" : [
+                                      {
+                                          "name":"cpnprdazurefile",
+                                          "mountPath":"/mnt/cpmodeldata"
+                                      }
+                                  ]                              
+                              }
+                          },      
+                          image="cpnprdacr.azurecr.io/test/a:v1",
+                          image_pull_policy='Always',
+                          resources={'request_memory': '22Gi', 'limit_memory': '24Gi'},      
+                          image_pull_secrets='cpnprdacr',
+                          arguments=["/mnt/cpmodeldata/ModelData/a/IOREPAIRVOLUME_Forecasting_Model.R","DEV","YES","NO","NO","NO","7","RPR_CABLE&RPRIP_DDCIM"],
+                          labels={"foo": "bar"},
+                          name="io_4",
+                          task_id="io_4",
+                          get_logs=True,
+                          volumes=[volume],
+                          volume_mounts=[volume_mount],                   
+                          dag=dag
+                          )
+
+io_5 = KubernetesPodOperator(namespace='default',
+                            executor_config={                            
+                              "KubernetesExecutor": {
+                                  "volumes": [
+                                      {
+                                       "name": "cpnprdazurefile",
+                                       "azureFile" :
+                                       {
+                                          "secretName":"cpnprdfilesharepv",
+                                          "shareName":"cpmodeldata"
+                                       }
+                                      }
+                                  ],
+                                  "volume_mounts" : [
+                                      {
+                                          "name":"cpnprdazurefile",
+                                          "mountPath":"/mnt/cpmodeldata"
+                                      }
+                                  ]                              
+                              }
+                          },      
+                          image="cpnprdacr.azurecr.io/test/a:v1",
+                          image_pull_policy='Always',
+                          resources={'request_memory': '22Gi', 'limit_memory': '24Gi'},      
+                          image_pull_secrets='cpnprdacr',
+                          arguments=["/mnt/cpmodeldata/ModelData/a/IOREPAIRVOLUME_Forecasting_Model.R","DEV","YES","NO","NO","NO","7","RPRAS_LEGACY&RPROOS_LEGACY"],
+                          labels={"foo": "bar"},
+                          name="io_5",
+                          task_id="io_5",
+                          get_logs=True,
+                          volumes=[volume],
+                          volume_mounts=[volume_mount],                   
+                          dag=dag
+                          )
+
+io_6 = KubernetesPodOperator(namespace='default',
+                            executor_config={                            
+                              "KubernetesExecutor": {
+                                  "volumes": [
+                                      {
+                                       "name": "cpnprdazurefile",
+                                       "azureFile" :
+                                       {
+                                          "secretName":"cpnprdfilesharepv",
+                                          "shareName":"cpmodeldata"
+                                       }
+                                      }
+                                  ],
+                                  "volume_mounts" : [
+                                      {
+                                          "name":"cpnprdazurefile",
+                                          "mountPath":"/mnt/cpmodeldata"
+                                      }
+                                  ]                              
+                              }
+                          },      
+                          image="cpnprdacr.azurecr.io/test/a:v1",
+                          image_pull_policy='Always',
+                          resources={'request_memory': '22Gi', 'limit_memory': '24Gi'},      
+                          image_pull_secrets='cpnprdacr',
+                          arguments=["/mnt/cpmodeldata/ModelData/a/IOREPAIRVOLUME_Forecasting_Model.R","DEV","YES","NO","NO","NO","7","RPR_UVHLP&RPR_IP_WORKSHARE"],
+                          labels={"foo": "bar"},
+                          name="io_6",
+                          task_id="io_6",
+                          get_logs=True,
+                          volumes=[volume],
+                          volume_mounts=[volume_mount],                   
+                          dag=dag
+                          )
+io_7 = KubernetesPodOperator(namespace='default',
+                            executor_config={                            
+                              "KubernetesExecutor": {
+                                  "volumes": [
+                                      {
+                                       "name": "cpnprdazurefile",
+                                       "azureFile" :
+                                       {
+                                          "secretName":"cpnprdfilesharepv",
+                                          "shareName":"cpmodeldata"
+                                       }
+                                      }
+                                  ],
+                                  "volume_mounts" : [
+                                      {
+                                          "name":"cpnprdazurefile",
+                                          "mountPath":"/mnt/cpmodeldata"
+                                      }
+                                  ]                              
+                              }
+                          },      
+                          image="cpnprdacr.azurecr.io/test/a:v1",
+                          image_pull_policy='Always',
+                          resources={'request_memory': '22Gi', 'limit_memory': '24Gi'},      
+                          image_pull_secrets='cpnprdacr',
+                          arguments=["/mnt/cpmodeldata/ModelData/a/IOREPAIRVOLUME_Forecasting_Model.R","DEV","YES","NO","NO","NO","7","RPR_IP_NONWORKSHARE&RPR_DTV_FWL"],
+                          labels={"foo": "bar"},
+                          name="io_7",
+                          task_id="io_7",
+                          get_logs=True,
+                          volumes=[volume],
+                          volume_mounts=[volume_mount],                   
+                          dag=dag
+                          )
 io_1.set_upstream(start)
 io_2.set_upstream(start)
 io_3.set_upstream(start)
 
-
+io_4.set_upstream(start)
+io_5.set_upstream(start)
+io_6.set_upstream(start)
+io_7.set_upstream(start)
